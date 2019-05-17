143557ac46d2bf547ef83bc7bddecdbef43938d0
amigocloud <robert.arzola.c@gmail.com>
2019-05-16 21:38:50 -0500

    edit files

--- config/settings/development.py
+++ config/settings/development.py
@@ -12,3 +12,5 @@ INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
 # Hack to have debug toolbar when developing with docker
 ip = socket.gethostbyname(socket.gethostname())
 INTERNAL_IPS += [ip[:-1] + "1"]
+
+SITE_ID = 1
