098f071ad40c525b825a0ffab1b4bc238e00ec69
amigocloud <robert.arzola.c@gmail.com>
2019-05-29 14:09:26 -0500

    add load_file_static_nginx

--- config/settings/default.py
+++ config/settings/default.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ''),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', ''),
-ALLOWED_HOSTS = 'django', '0.0.0.0',
+ALLOWED_HOSTS = 'django', '0.0.0.0', '127.0.0.1', 'localhost',
 
 
 # Application definition
